f4442128686b9652417eef4795651bb6dec3b956
Dudi001 <murad.manapov.96@gmail.com>
2023-02-27 14:21:09 +0300

    need fix storyboard

---
+++ MyImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageListCell.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 24.02.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImageListCell"
+    
+}
---
+++ MyImageFeed/ImageList/ImagesListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 18.02.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var ImageList: UITableView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configCell(for cell: ImagesListCell) { }
+
+}
+
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Получаем ячейку из определенного типа
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier,
+            for: indexPath
+        )
+        
+        //приводим к нужному нам типу
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("We have a problem")
+            return UITableViewCell()
+        }
+        
+        //конфигурируем и возвращаем
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+    
+    
+}
+
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
