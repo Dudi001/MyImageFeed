8578c5a265eb0d1642d53d51153978ae89b68e07
Dudi001 <murad.manapov.96@gmail.com>
2023-03-05 19:14:19 +0300

    add: Outlet and action

--- MyImageFeed/Profile/ProfileViewController.swift
+++ MyImageFeed/Profile/ProfileViewController.swift
@@ -9,4 +9,13 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
 }
