025c587cd1dfbb20fe8ae45e00dfcdb8e7ea7ba0
Dudi001 <murad.manapov.96@gmail.com>
2023-03-05 18:54:21 +0300

    add: new profile view controller

---
+++ MyImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 04.03.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
