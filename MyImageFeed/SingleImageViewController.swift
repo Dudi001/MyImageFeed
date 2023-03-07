76f97423ee609e1c504cf3323ce5cf6640ae0b8b
Dudi001 <murad.manapov.96@gmail.com>
2023-03-07 19:01:37 +0300

    add: new controller for single image

---
+++ MyImageFeed/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 07.03.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var ImageView: UIImageView!
+    
+    
+}
