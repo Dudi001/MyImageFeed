4c7e9cd5fb3d006ba9c697bc20f49c8df12c58b1
Dudi001 <murad.manapov.96@gmail.com>
2023-03-08 21:18:50 +0300

    add: change image from singleViewCotroller

--- MyImageFeed/ImageList/ImagesListViewController.swift
+++ MyImageFeed/ImageList/ImagesListViewController.swift
@@ -12,7 +12,6 @@ final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -20,10 +19,23 @@ final class ImagesListViewController: UIViewController {
         return formatter
     }()
     
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSingleImage" {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -61,6 +73,7 @@ extension ImagesListViewController: UITableViewDataSource {
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "ShowSingleImage", sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
--- MyImageFeed/SingleImageView/SingleImageViewController.swift
+++ MyImageFeed/SingleImageView/SingleImageViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage?
     
-    @IBOutlet var ImageView: UIImageView!
+    @IBOutlet var imageView: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
     
 }
