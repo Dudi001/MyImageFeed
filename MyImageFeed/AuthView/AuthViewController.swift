fc6dd82550b11c09d9001b12ee66646c962f504e
Dudi001 <murad.manapov.96@gmail.com>
2023-03-24 11:41:31 +0300

    add: Auth and Web viewControllers, delegate

---
+++ MyImageFeed/AuthView/AuthViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AuthViewController.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 23.03.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    @IBOutlet private var loginButton: UIButton!
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
---
+++ MyImageFeed/Unsplash/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyImageFeed
+//
+//  Created by Мурад Манапов on 18.03.2023.
+//
+
+import Foundation
+
+
+let AccessKey = "1Lb6Q3vvSatZvUHxwByOv8G32vKP_aADyB0S5pqhKzg"
+let SecretKey = "Oa7Vc3APhdnv66Dy2seavgdkSXmwDpSITD3bubCD-v4"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
